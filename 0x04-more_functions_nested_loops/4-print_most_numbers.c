78d0815527c909394a2bfc4b1fccbffbd8537ded
emanalaaa <90788783+emanalaaa@users.noreply.github.com>
2023-03-14 21:31:46 +0200

    ..

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
+*print_most_numbers - prints
 * owned by Bwave ICT
 *Return: void
 */
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if ((c != '2' || c != '4'))
+	if (!(c == '2' || c == '4'))
 	_putchar(c);
 	}
 	_putchar('\n');
