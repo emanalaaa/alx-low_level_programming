07b54ad8676e79c71055c6f1d0dda2b020b2c2e5
emanalaaa <90788783+emanalaaa@users.noreply.github.com>
2023-03-14 21:25:37 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,9 @@ void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9' && c != 4 && c != 2; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
+		if ((c != '2' || c != '4'))
 	_putchar(c);
 	}
 	_putchar('\n');
